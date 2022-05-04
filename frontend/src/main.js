51d2c02efa98e20803ad4c1082153b15cf4042e5
‘Fabrice <fabrice.avrila@gmail.com>
2022-05-04 18:28:33 +0400

    package json

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,4 @@
+/*
 import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
@@ -10,6 +11,29 @@ import router from "./router";
 import VueScrollFixedNavbar from "vue-scroll-fixed-navbar";
 import Meta from "vue-meta";
 Vue.use(Meta);
+Vue.use(VueScrollFixedNavbar);
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
+*/
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import router from "./router";
+import VueScrollFixedNavbar from "vue-scroll-fixed-navbar";
+
 Vue.use(VueScrollFixedNavbar);
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
