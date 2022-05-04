0c02a15114c448a872ac66bf40a9c37c133d9f08
‘Fabrice <fabrice.avrila@gmail.com>
2022-05-04 14:37:40 +0400

    sitemap

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 const SitemapPlugin = require("sitemap-webpack-plugin").default;
 // You can write the paths as an array of strings or an array of objects
-
+/*
 const paths = [
   {
     path: "/",
@@ -33,7 +33,7 @@ const paths = [
     changefreq: "yearly",
   },
 ];
-
+*/
 module.exports = {
   productionSourceMap: false,
   devServer: {
@@ -48,8 +48,10 @@ module.exports = {
       },
     },
   },
+  /*
   configureWebpack: {
     plugins: [new SitemapPlugin({ base: "https://kareshmaart.com", paths })],
   },
+  */
   outputDir: "../client-build",
 };
