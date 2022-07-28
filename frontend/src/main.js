688c26f3446880afaae560e97bbd2325c2883f8e
‘Fabrice <fabrice.avrila@gmail.com>
2022-07-28 16:29:16 +0400

    vue config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,3 @@
-/*
-import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-import App from "./App.vue";
-import VueRouter from "vue-router";
-import router from "./router";
-import VueScrollFixedNavbar from "vue-scroll-fixed-navbar";
-import Meta from "vue-meta";
-Vue.use(Meta);
-Vue.use(VueScrollFixedNavbar);
-Vue.use(VueRouter);
-Vue.config.productionTip = false;
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
-*/
 import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,59 +1,9 @@
 //const SitemapPlugin = require("sitemap-webpack-plugin").default;
 // You can write the paths as an array of strings or an array of objects
-/*
-const paths = [
-  {
-    path: "/",
-    lastmod: "2021-11-22",
-    priority: 1.0,
-    changefreq: "yearly",
-  },
-  {
-    path: "/Tableaux",
-    lastmod: "2021-11-22",
-    priority: 1.0,
-    changefreq: "yearly",
-  },
-  {
-    path: "/blog",
-    lastmod: "2021-11-22",
-    priority: 1.0,
-    changefreq: "yearly",
-  },
-  {
-    path: "/moi",
-    lastmod: "2021-11-22",
-    priority: 1.0,
-    changefreq: "yearly",
-  },
-  {
-    path: "/contact",
-    lastmod: "2021-11-22",
-    priority: 1.0,
-    changefreq: "yearly",
-  },
-];
-*/
 module.exports = {
   productionSourceMap: false,
   devServer: {
-    proxy: "https://kareshmaart.com/api/",
+    proxy: "https://www.kareshmaart.com/api/",
   },
-  /*
-  devServer: {
-    proxy: {
-      "^/api": {
-        target: "URL",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
-  },
-  */
-  /*
-  configureWebpack: {
-    plugins: [new SitemapPlugin({ base: "https://kareshmaart.com", paths })],
-  },
-*/
   outputDir: "../client-build",
 };
