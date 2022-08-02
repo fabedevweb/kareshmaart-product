16f9de212317e84d86afb3e766e2d27a7310a4aa
‘Fabrice <fabrice.avrila@gmail.com>
2022-08-02 22:41:55 +0400

    hashtag

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,123 +16,122 @@ import Cgu from "../views/Cgu.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/Tableaux",
-    name: "Tableaux",
-    component: Tableaux,
-    children: [
-      {
-        path: "/MyInspiration",
-        component: MyInspiration,
-      },
-      {
-        path: "/YourInspiration",
-        component: YourInspiration,
-      },
-    ],
-  },
-  {
-    path: "/blog",
-    name: "Blog",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Blog.vue"),
-  },
-  {
-    path: "/moi",
-    name: "Moi",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Moi.vue"),
-  },
-  {
-    path: "/contact",
-    name: "Contact",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
-  },
-  {
-    path: "/admin",
-    name: "Admin",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
-  },
-  {
-    path: "/superadmin",
-    name: "SuperAdmin",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/SuperAdmin.vue"),
-  },
-
-  {
-    path: "/create",
-    name: "Create",
-    component: Create,
-  },
-  {
-    path: "/newCanvasMe",
-    name: "NewCanvasMe",
-    component: NewCanvasMe,
-  },
-  {
-    path: "/newCanvasYour",
-    name: "NewCanvasYour",
-    component: NewCanvasYour,
-  },
-  {
-    path: "/newArticle",
-    name: "NewArticle",
-    component: NewArticle,
-  },
-  {
-    path: "/setCanvasMe",
-    name: "SetCanvasMe",
-    component: SetCanvasMe,
-  },
-  {
-    path: "/setCanvasYour",
-    name: "SetCanvasYour",
-    component: SetCanvasYour,
-  },
-  {
-    path: "/setMyBlog",
-    name: "SetMyBlog",
-    component: SetMyBlog,
-  },
-  {
-    path: "/mentions",
-    name: "Mentions",
-    component: Mentions,
-  },
-  {
-    path: "/cgu",
-    name: "Cgu",
-    component: Cgu,
-  },
-];
-
 const router = new VueRouter({
-  //mode: "history",
+  mode: "history",
   //base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/Tableaux",
+      name: "Tableaux",
+      component: Tableaux,
+      children: [
+        {
+          path: "/MyInspiration",
+          component: MyInspiration,
+        },
+        {
+          path: "/YourInspiration",
+          component: YourInspiration,
+        },
+      ],
+    },
+    {
+      path: "/blog",
+      name: "Blog",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/Blog.vue"),
+    },
+    {
+      path: "/moi",
+      name: "Moi",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/Moi.vue"),
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
+    },
+    {
+      path: "/admin",
+      name: "Admin",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
+    },
+    {
+      path: "/superadmin",
+      name: "SuperAdmin",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/SuperAdmin.vue"),
+    },
+
+    {
+      path: "/create",
+      name: "Create",
+      component: Create,
+    },
+    {
+      path: "/newCanvasMe",
+      name: "NewCanvasMe",
+      component: NewCanvasMe,
+    },
+    {
+      path: "/newCanvasYour",
+      name: "NewCanvasYour",
+      component: NewCanvasYour,
+    },
+    {
+      path: "/newArticle",
+      name: "NewArticle",
+      component: NewArticle,
+    },
+    {
+      path: "/setCanvasMe",
+      name: "SetCanvasMe",
+      component: SetCanvasMe,
+    },
+    {
+      path: "/setCanvasYour",
+      name: "SetCanvasYour",
+      component: SetCanvasYour,
+    },
+    {
+      path: "/setMyBlog",
+      name: "SetMyBlog",
+      component: SetMyBlog,
+    },
+    {
+      path: "/mentions",
+      name: "Mentions",
+      component: Mentions,
+    },
+    {
+      path: "/cgu",
+      name: "Cgu",
+      component: Cgu,
+    },
+  ],
 });
 
 export default router;
