884addf8750100e826c331341c673aa755c1902f
‘Fabrice <fabrice.avrila@gmail.com>
2022-08-03 12:18:57 +0400

    delete comment route lazy

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,115 +15,79 @@ const router = new VueRouter({
     },
     {
       path: "/Tableaux",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Tableaux.vue"),
+      component: () => import("../views/Tableaux.vue"),
       children: [
         {
           path: "/MyInspiration",
-          component: () =>
-            import(
-              /* webpackChunkName: "about" */ "../views/MyInspiration.vue"
-            ),
+          component: () => import("../views/MyInspiration.vue"),
         },
         {
           path: "/YourInspiration",
-          component: () =>
-            import(
-              /* webpackChunkName: "about" */ "../views/YourInspiration.vue"
-            ),
+          component: () => import("../views/YourInspiration.vue"),
         },
       ],
     },
     {
       path: "/blog",
       name: "Blog",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Blog.vue"),
+      component: () => import("../views/Blog.vue"),
     },
     {
       path: "/moi",
       name: "Moi",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Moi.vue"),
+      component: () => import("../views/Moi.vue"),
     },
     {
       path: "/contact",
       name: "Contact",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
+      component: () => import("../views/Contact.vue"),
     },
     {
       path: "/admin",
       name: "Admin",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
+      component: () => import("../views/Admin.vue"),
     },
     {
       path: "/superadmin",
       name: "SuperAdmin",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/SuperAdmin.vue"),
+      component: () => import("../views/SuperAdmin.vue"),
     },
 
     {
       path: "/create",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Create.vue"),
+      component: () => import("../views/Create.vue"),
     },
     {
       path: "/newCanvasMe",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/NewCanvasMe.vue"),
+      component: () => import("../views/NewCanvasMe.vue"),
     },
     {
       path: "/newCanvasYour",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/NewCanvasYour.vue"),
+      component: () => import("../views/NewCanvasYour.vue"),
     },
     {
       path: "/newArticle",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/NewArticle.vue"),
+      component: () => import("../views/NewArticle.vue"),
     },
     {
       path: "/setCanvasMe",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/SetCanvasMe.vue"),
+      component: () => import("../views/SetCanvasMe.vue"),
     },
     {
       path: "/setCanvasYour",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/SetCanvasYour.vue"),
+      component: () => import("../views/SetCanvasYour.vue"),
     },
     {
       path: "/setMyBlog",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/SetMyBlog.vue"),
+      component: () => import("../views/SetMyBlog.vue"),
     },
     {
       path: "/mentions",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Mentions.vue"),
+      component: () => import("../views/Mentions.vue"),
     },
     {
       path: "/cgu",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/Cgu.vue"),
+      component: () => import("../views/Cgu.vue"),
     },
   ],
 });
